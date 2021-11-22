8f39505a0ca6d7eec2e2a7ac4695f3ae76bf2981
849961 <849961@SAIT227700IT.ACDM.DS.SAIT.CA>
2021-11-21 18:33:41 -0700

    account service java class

---
+++ src/java/services/AccountService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+import dataaccess.UserDB;
+import models.User;
+/**
+ *
+ * @author 849961
+ */
+public class AccountService {
+    
+    public User login(String email, String password) {
+        UserDB userDB = new UserDB();
+        try {
+            User user = userDB.get(email);
+            if (password.equals(user.getPassword())) {
+                return user;
+            }
+        } catch (Exception e) {
+        }
+        
+        return null;
+    }
+}
